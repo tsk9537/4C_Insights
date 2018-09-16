b67412f722cab66ead855ab989e0ccfca58c7eaf
tsk9537 <37111501+tsk9537@users.noreply.github.com>
2018-09-15 22:01:31 -0500

    Add files via upload

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: nginx
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package ['nginx'] do
+  action :install
+end
+
+package ['php7.0-fpm'] do
+  action :install
+end
+
+package ['php-imagick', 'php-curl', 'php-gd', 'php-mcrypt', 'php-xml', 'php-mbstring', 'php-soap', 'php-mysql', 'php-pear'] do
+  action :install
+end
+
+package ['ntp', 'htop', 'vim'] do
+  action :install
+end
+
+template '/etc/nginx/sites-enabled/default' do
+  source 'default.conf'
+end
